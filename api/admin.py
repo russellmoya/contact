c6bd1d4b388828aa8388418672b7b24e2a20dcac
russellmoya <moyarussell10@gmail.com>
2019-10-05 15:35:25 +0100

    commit

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Personnel,News,ListNumero,Entreprise,Departement
 # Register your models here.
+admin.register(Personnel,News,Entreprise,Departement,ListNumero)
